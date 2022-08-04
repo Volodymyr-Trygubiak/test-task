37b88ec5183ce94ea5593a8874ae10abd2a00832
Volodymyr-Trygubiak <trigubiak@gmail.com>
2022-08-04 15:51:44 +0200

    change title from h1 to h2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <main>
       <div className='main-wrap'>
-        <h1>Test assignment for front-end developer</h1>
+        <h2>Test assignment for front-end developer</h2>
         <p className='main-text'>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</p>
         <Button>Sign Up</Button>
       </div>
